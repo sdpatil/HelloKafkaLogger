41e2c36150c3ceba05a7b3486e5d6d5a5ecd901d
Sunil Patil <sunilpatil@maprtech.com>
2016-11-22 13:14:34 -0800

    Changes in log configuration

--- src/main/java/com/spnotes/kafka/HelloKafkaLogger.java
+++ src/main/java/com/spnotes/kafka/HelloKafkaLogger.java
@@ -4,7 +4,6 @@ package com.spnotes.kafka;
 import org.apache.log4j.LogManager;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.slf4j.impl.Log4jLoggerFactory;
 
 /**
  * Created by sunilpatil on 11/7/16.
@@ -12,17 +11,15 @@ import org.slf4j.impl.Log4jLoggerFactory;
 public class HelloKafkaLogger {
     private static final Logger logger = LoggerFactory.getLogger(HelloKafkaLogger.class);
 
-    public static void main(String[] argv){
+    public static void main(String[] argv) {
         System.out.println("Inside HelloKafkaLogger.main");
 
-        for(int i = 10 ;i < 20; i++){
-            logger.warn("Debug message from HelloKafkaLogger.main " + i);
-        }
-        try {
-            Thread.sleep(1000);
-        }catch (Exception ex){
 
-        }
+            logger.debug("Debug message from HelloKafkaLogger.main," );
+            logger.info("Info message from HelloKafkaLogger.main" );
+            logger.warn("Warn message from HelloKafkaLogger.main");
+            logger.error("Error message from HelloKafkaLogger.main" );
+
         System.out.println("Exiting HelloKafkaLogger.main");
         LogManager.shutdown();
     }
